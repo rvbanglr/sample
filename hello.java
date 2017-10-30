ab8ce48bd800de950bef17c8c90f54d2468770a7
ravi <rv.banglr@gmail.com>
2017-10-30 05:49:37 -0700

    New Jenkins integration

--- hello.java
+++ hello.java
@@ -2,6 +2,7 @@
 public class hello{
 	public static void main(String[] args){
 		for(int i =1; i<=10; i++){
+			System.out.println("Jenkins integration started 10/30")
 			System.out.println("Hellow world.."+i);
 		}
 	}
