7498c2c03b402f247ca945fefc641a5511f44d61
ravi <rv.banglr@gmail.com>
2017-10-30 20:04:02 -0700

    jenkins git integration after codee changes

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 //Initial code 10/30
 public class hello{
 	public static void main(String[] args){
-		System.out.println("Jenkins git integration started 10/30);
+		System.out.println("Jenkins git integration started 10/30");
 		for(int i =1; i<=10; i++){
 			System.out.println("Hellow world.."+i);
 		}
